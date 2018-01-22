8f0945d0db7ba4c48b7554303840c0dff8a0e4f9
andreepratama27 <andreepratama27@gmail.com>
2018-01-22 15:34:58 +0700

    change to bulma

--- test/test.js
+++
@@ -1,9 +0,0 @@
-const assert = require('chai').assert
-
-describe('array', () => {
-    describe('#indexOf', () => {
-        it('should return -1 when the value isn\'t present', () => {
-            assert.equal(-1, [1,2,3].indexOf(4))
-        })
-    })
-})
